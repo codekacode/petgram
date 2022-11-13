c27d6300a417cd8269f1a99e02a1bbfa108399c6
Codekacode <karem.quiroz.m@gmail.com>
2022-11-13 16:49:32 -0500

    Add lis of categories fixed

--- petgram-front/src/components/ListOfCategories.jsx
+++ petgram-front/src/components/ListOfCategories.jsx
@@ -4,6 +4,7 @@ import { Item, List } from './styles'
 
 function ListOfCategories() {
   const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
 
   useEffect(function(){
     fetch("https://petgram-server-edsf8xpy2.now.sh/categories")
@@ -12,10 +13,28 @@ function ListOfCategories() {
         setCategories(response)
       })
   }, [])
-  return (
-    <List>
-      {categories.map(cat => <Item><Category {...cat} /></Item>)}
+  
+  useEffect(function(){
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed)
+    }
+    document.addEventListener("scroll", onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : '' }>
+      {categories.map(cat => <Item key={cat.id} ><Category {...cat} /></Item>)}
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
 
