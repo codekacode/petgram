e22279135c28ee0ae8addc147f1bd64034149025
Codekacode <karem.quiroz.m@gmail.com>
2022-11-13 16:05:27 -0500

    Add fetch to get data of categories

--- petgram-front/src/App.js
+++ petgram-front/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import ListOfCategories from './components/ListOfCategories';
 import ListOfPhotoCards from './components/ListOfPhotoCards';
 import { Logo } from './components/Logo';
-import PhotoCard from './components/PhotoCard';
 import { GlobalStyle } from './GlobalStyles';
 
 function App() {
--- petgram-front/src/components/ListOfCategories.jsx
+++ petgram-front/src/components/ListOfCategories.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Category from './Category'
 import { Item, List } from './styles'
-import db from '../db.json'
 
 function ListOfCategories() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(function(){
+    fetch("https://petgram-server-edsf8xpy2.now.sh/categories")
+      .then(res => res.json())
+      .then(response => {
+        setCategories(response)
+      })
+  }, [])
   return (
     <List>
-      {db.categories.map(cat => <Item><Category {...cat} /></Item>)}
+      {categories.map(cat => <Item><Category {...cat} /></Item>)}
     </List>
   )
 }
