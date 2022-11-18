8de7ae85eb0238ae4437e346ad4b43d2bc53f67f
Codekacode <karem.quiroz.m@gmail.com>
2022-11-18 00:07:44 -0500

    Add observer

--- petgram-front/src/components/PhotoCard.jsx
+++ petgram-front/src/components/PhotoCard.jsx
@@ -10,18 +10,22 @@ function PhotoCard({id, likes = 0, src = DEFAULT_IMAGE}) {
   const elementRef = useRef()
 
   useEffect(function () {
-    //esta o no en el viewport
-    const observer = new window.IntersectionObserver(
-      function(entries){
-        const {isIntersecting} = entries[0]
-        console.log({isIntersecting})
-        if(isIntersecting){
-          setShow(true)
-          observer.disconnect()
-        }
-      }
-    )
-    observer.observe(elementRef.current)
+    import('intersection-observer')
+      .then(()=> {
+        //esta o no en el viewport
+        const observer = new window.IntersectionObserver(
+          function(entries){
+            const {isIntersecting} = entries[0]
+            console.log({isIntersecting})
+            if(isIntersecting){
+              setShow(true)
+              observer.disconnect()
+            }
+          }
+        )
+        observer.observe(elementRef.current)
+      })
+    
   },[elementRef])
 
   return (
