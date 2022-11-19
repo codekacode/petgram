3efc5a718755a8d55a8895b15501c16012a65483
Codekacode <karem.quiroz.m@gmail.com>
2022-11-18 23:59:40 -0500

    Add liked

--- petgram-front/src/components/ListOfPhotoCards.jsx
+++ petgram-front/src/components/ListOfPhotoCards.jsx
@@ -5,7 +5,7 @@ import { ListOfPhotoCard } from './styles'
 function ListOfPhotoCards() {
   return (
     <ListOfPhotoCard>
-      {[1,2,3,4,5,6].map(id => <PhotoCard key={id}/>)}
+      {[1,2,3,4,5,6].map(id => <PhotoCard id={id} key={id} />)}
     </ListOfPhotoCard>
   )
 }
--- petgram-front/src/components/PhotoCard.jsx
+++ petgram-front/src/components/PhotoCard.jsx
@@ -1,14 +1,30 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { Article, PhotoCardButton, PhotoCardImg, PhotoCardImgWrapper } from './styles'
-import { MdFavoriteBorder } from "react-icons/md";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
 
 const DEFAULT_IMAGE = "https://res.cloudinary.com/midudev/image/upload/w_300/q_80/v1560262103/dogs.png"
 
 function PhotoCard({id, likes = 0, src = DEFAULT_IMAGE}) {
   const [show, setShow] = useState(false);
-
+  const key = `like-${id}`
+  const [liked, setLiked] = useState(() => {
+    try {
+      const like = window.localStorage.getItem(key);
+      return JSON.parse(like)
+    } catch (error) {
+      return false;
+    }
+  });
   const elementRef = useRef()
 
+  const setLocalStorage = value => {
+    try {
+      window.localStorage.setItem(key, value);
+      setLiked(value); 
+    } catch (error) {
+      console.log(error);
+    }
+  }
   useEffect(function () {
     import('intersection-observer')
       .then(()=> {
@@ -28,20 +44,24 @@ function PhotoCard({id, likes = 0, src = DEFAULT_IMAGE}) {
     
   },[elementRef])
 
+  const Icon = liked ? MdFavorite : MdFavoriteBorder;
   return (
     <Article ref={elementRef}>
-      {show && <>
-         <a href={`/detail/${id}`}>
-        <PhotoCardImgWrapper>
-          <PhotoCardImg src={src}/>
-        </PhotoCardImgWrapper>
-        </a>
-        <PhotoCardButton>
-          <MdFavoriteBorder size="32px"/>{likes} likes!
-        </PhotoCardButton>
-      </>}
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <PhotoCardImgWrapper>
+              <PhotoCardImg src={src} />
+            </PhotoCardImgWrapper>
+          </a>
+          <PhotoCardButton onClick={() => setLocalStorage(!liked)}>
+            <Icon size="32px" />
+            {likes} likes!
+          </PhotoCardButton>
+        </>
+      )}
     </Article>
-  )
+  );
 }
 
 export default PhotoCard
